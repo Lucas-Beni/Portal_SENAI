a87926bf8bc240a2a65e4667c9f05a51552788d3
Lucas-Beni <lucgarcbeni@gmail.com>
2024-11-21 20:14:12 -0300

    efeitos da pagina de adicionar prontos

--- adicionar.js
+++ adicionar.js
@@ -1,9 +1,13 @@
 const aluno = document.getElementById("alunos")
 const turma = document.getElementById("turmas")
 const atividade = document.getElementById("atividades")
+const disciplina = document.getElementById("disciplinas")
 
 function aluno1(){
-    aluno.innerHTML =  `<div class="inputs">
+    aluno.innerHTML =  `<div class="nome">
+                            <p>Alunos</p>
+                        </div>
+                        <div class="inputs">
                             <input type="text" placeholder="Usuário">
                             <input type="password" placeholder="Senha">
                             <input type="email" placeholder="E-mail">
@@ -21,10 +25,13 @@ function aluno1(){
     atividade.style.left = "50%"
     atividade.style.top = "85%"
     atividade.style.transition = ".5s"
+    disciplina.style.left = "75%"
+    disciplina.style.top = "75%"
+    disciplina.style.transition = ".5s"
 }
 
 function aluno2(){
-    aluno.innerHTML = ``
+    aluno.innerHTML = `<p>Alunos</p>`
     aluno.style.width = "10vw"
     aluno.style.height = "20vh"
     aluno.style.backgroundImage = "none"
@@ -33,9 +40,13 @@ function aluno2(){
 alunos.addEventListener('click', aluno1);
 alunos.addEventListener('click', turma2)
 alunos.addEventListener('click', atividade2)
+alunos.addEventListener('click', disciplina2);
 
 function turma1(){
-    turma.innerHTML =  `<div class="inputs">
+    turma.innerHTML =  `<div class="nome">
+                            <p>Turmas</p>
+                        </div>
+                        <div class="inputs">
                             <input type="text" placeholder="Usuário">
                             <input type="password" placeholder="Senha">
                             <input type="email" placeholder="E-mail">
@@ -53,10 +64,13 @@ function turma1(){
     atividade.style.left = "50%"
     atividade.style.top = "85%"
     atividade.style.transition = ".5s"
+    disciplina.style.left = "75%"
+    disciplina.style.top = "75%"
+    disciplina.style.transition = ".5s"
 }
 
 function turma2(){
-    turma.innerHTML = ``
+    turma.innerHTML = `<p>Turmas</p>`
     turma.style.width = "10vw"
     turma.style.height = "20vh"
     turma.style.backgroundImage = "none"
@@ -65,9 +79,13 @@ function turma2(){
 turma.addEventListener('click', turma1);
 turma.addEventListener('click', aluno2);
 turma.addEventListener('click', atividade2)
+turma.addEventListener('click', disciplina2);
 
 function atividade1(){
-    atividade.innerHTML =  `<div class="inputs">
+    atividade.innerHTML = `<div class="nome">
+                            <p>Atividades</p>
+                        </div>
+                        <div class="inputs">
                             <input type="text" placeholder="Usuário">
                             <input type="password" placeholder="Senha">
                             <input type="email" placeholder="E-mail">
@@ -82,13 +100,16 @@ function atividade1(){
     aluno.style.left = "25%"
     aluno.style.top = "75%"
     aluno.style.transition = ".5s"
-    turma.style.left = "50%"
-    turma.style.top = "85%"
+    turma.style.left = "75%"
+    turma.style.top = "75%"
     turma.style.transition = ".5s"
+    disciplina.style.left = "50%"
+    disciplina.style.top = "85%"
+    disciplina.style.transition = ".5s"
 }
 
 function atividade2(){
-    atividade.innerHTML = ``
+    atividade.innerHTML = `<p>Atividades</p>`
     atividade.style.width = "10vw"
     atividade.style.height = "20vh"
     atividade.style.backgroundImage = "none"
@@ -96,4 +117,44 @@ function atividade2(){
 
 atividade.addEventListener('click', atividade1);
 atividade.addEventListener('click', aluno2);
-atividade.addEventListener('click', turma2)
+atividade.addEventListener('click', turma2);
+atividade.addEventListener('click', disciplina2);
+
+function disciplina1(){
+    disciplina.innerHTML = `<div class="nome">
+                                <p>Disciplinas</p>
+                            </div>
+                            <div class="inputs">
+                                <input type="text" placeholder="Usuário">
+                                <input type="password" placeholder="Senha">
+                                <input type="email" placeholder="E-mail">
+                            </div>`
+    disciplina.style.left = "50%"
+    disciplina.style.top = "50%"
+    disciplina.style.backgroundImage = "url('img/fundoLogin.jpg')"
+    disciplina.style. backgroundSize = "cover"
+    disciplina.style.width = "30vw"
+    disciplina.style.height = "45vh"
+    disciplina.style.transition = ".5s"
+    aluno.style.left = "25%"
+    aluno.style.top = "75%"
+    aluno.style.transition = ".5s"
+    turma.style.left = "75%"
+    turma.style.top = "75%"
+    turma.style.transition = ".5s"
+    atividade.style.left = "50%"
+    atividade.style.top = "85%"
+    atividade.style.transition = ".5s"
+}
+
+function disciplina2(){
+    disciplina.innerHTML = `<p>Disciplinas</p>`
+    disciplina.style.width = "10vw"
+    disciplina.style.height = "20vh"
+    disciplina.style.backgroundImage = "none"
+}
+
+disciplina.addEventListener('click', disciplina1);
+disciplina.addEventListener('click', aluno2);
+disciplina.addEventListener('click', turma2);
+disciplina.addEventListener('click', atividade2);
