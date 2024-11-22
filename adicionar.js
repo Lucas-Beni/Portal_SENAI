4144e8b6a6964ed38d72ac0b48d0a77aa1dbc429
Lucas Beni <lucgarcbeni@gmail.com>
2024-11-22 16:54:49 -0300

    erros da tela de adicionar arrumados

--- adicionar.js
+++ adicionar.js
@@ -3,15 +3,36 @@ const turma = document.getElementById("turmas");
 const atividade = document.getElementById("atividades");
 const disciplina = document.getElementById("disciplinas");
 
+aluno.addEventListener('click', alunoLarge)
+turma.addEventListener('click', turmaLarge)
+atividade.addEventListener('click', atividadeLarge)
+disciplina.addEventListener('click', disciplinaLarge)
+
 function aluno1() {
     aluno.innerHTML = `<div class="nome">
-                            <p>Alunos</p>
-                        </div>
-                        <div class="inputs">
-                            <input type="text" placeholder="Usuário">
-                            <input type="password" placeholder="Senha">
-                            <input type="email" placeholder="E-mail">
-                        </div>`;
+            <p>Alunos</p>
+        </div>
+        <div class="inputs-1">
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" placeholder="Nome" required="required">
+            <label for="cpf">CPF</label>
+            <input type="text" id="cpf" placeholder="Ex: 999.999.999-99" required="required">
+        </div>
+        <div class="inputs-2">
+            <label for="email">E-mail</label>
+            <input type="email" id="email" placeholder="E-mail" required="required">
+            <label for="telefone">Telefone</label>
+            <input type="tel" id="telefone" class="input-padrao" required placeholder="(xx) xxxxx-xxxx" required="required">
+        </div>
+        <div class="select-turma">
+            <label for="turma">Turma</label>
+            <select name="turma" id="turma">
+                <option value=""></option>
+                <option value="">DEV-2</option>
+                <option value="">Logística</option>
+            </select>
+        <button>Confirmar</button>
+        </div>`;
     aluno.style.left = "50%";
     aluno.style.top = "50%";
     aluno.style.backgroundImage = "url('img/fundoLogin.jpg')";
@@ -19,6 +40,7 @@ function aluno1() {
     aluno.style.width = "30vw";
     aluno.style.height = "45vh";
     aluno.style.transition = ".5s";
+    aluno.style.border=".3vw solid #D9D9D9"
     turma.style.left = "25%";
     turma.style.top = "75%";
     turma.style.transition = ".5s";
@@ -28,6 +50,7 @@ function aluno1() {
     disciplina.style.left = "75%";
     disciplina.style.top = "75%";
     disciplina.style.transition = ".5s";
+    aluno.removeEventListener('click',alunoLarge)
 }
 
 function aluno2() {
@@ -35,18 +58,32 @@ function aluno2() {
     aluno.style.width = "10vw";
     aluno.style.height = "20vh";
     aluno.style.backgroundImage = "none";
+    aluno.style.border=".3vw solid #B50709"
+    aluno.addEventListener('click', alunoLarge)
+    
 }
 
-// Adiciona ouvintes diretamente
-aluno.addEventListener('click', (event) => {
-    // Garante que apenas cliques fora dos inputs disparem as funções
-    if (event.target.tagName !== 'INPUT') {
+function alunoLarge (event){
+    if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'SELECT' && event.target.tagName !== 'BUTTON') {
         aluno1();
         turma2();
         atividade2();
         disciplina2();
     }
-});
+}
+
+    // (event) => {
+    // // Garante que apenas cliques fora dos inputs ou selects disparem a função
+    // if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'SELECT' && event.target.tagName !== 'BUTTON') {
+    //     aluno1();
+    //     turma2();
+    //     atividade2();
+    //     disciplina2();
+    // }
+// });
+
+// Adiciona ouvintes diretamente
+
 
 function turma1() {
     turma.innerHTML = `<div class="nome">
@@ -64,6 +101,7 @@ function turma1() {
     turma.style.width = "30vw";
     turma.style.height = "45vh";
     turma.style.transition = ".5s";
+    turma.style.border=".3vw solid #D9D9D9";
     aluno.style.left = "25%";
     aluno.style.top = "75%";
     aluno.style.transition = ".5s";
@@ -73,6 +111,7 @@ function turma1() {
     disciplina.style.left = "75%";
     disciplina.style.top = "75%";
     disciplina.style.transition = ".5s";
+    turma.removeEventListener('click',turmaLarge)
 }
 
 function turma2() {
@@ -80,16 +119,27 @@ function turma2() {
     turma.style.width = "10vw";
     turma.style.height = "20vh";
     turma.style.backgroundImage = "none";
+    turma.style.border=".3vw solid #B50709"
+    turma.addEventListener('click',turmaLarge)
 }
 
-turma.addEventListener('click', (event) => {
-    if (event.target.tagName !== 'INPUT') {
+function turmaLarge (event){
+    if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'SELECT' && event.target.tagName !== 'BUTTON') {
         turma1();
         aluno2();
         atividade2();
         disciplina2();
     }
-});
+}
+
+// turma.addEventListener('click', (event) => {
+//     if (event.target.tagName !== 'INPUT') {
+//         turma1();
+//         aluno2();
+//         atividade2();
+//         disciplina2();
+//     }
+// });
 
 function atividade1() {
     atividade.innerHTML = `<div class="nome">
@@ -107,6 +157,7 @@ function atividade1() {
     atividade.style.width = "30vw";
     atividade.style.height = "45vh";
     atividade.style.transition = ".5s";
+    atividade.style.border=".3vw solid #D9D9D9";
     aluno.style.left = "25%";
     aluno.style.top = "75%";
     aluno.style.transition = ".5s";
@@ -116,6 +167,7 @@ function atividade1() {
     disciplina.style.left = "50%";
     disciplina.style.top = "85%";
     disciplina.style.transition = ".5s";
+    atividade.removeEventListener('click',atividadeLarge)
 }
 
 function atividade2() {
@@ -123,16 +175,27 @@ function atividade2() {
     atividade.style.width = "10vw";
     atividade.style.height = "20vh";
     atividade.style.backgroundImage = "none";
+    atividade.style.border=".3vw solid #B50709";
+    atividade.addEventListener('click',atividadeLarge)
 }
 
-atividade.addEventListener('click', (event) => {
-    if (event.target.tagName !== 'INPUT') {
+function atividadeLarge (event){
+    if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'SELECT' && event.target.tagName !== 'BUTTON') {
         atividade1();
         aluno2();
         turma2();
         disciplina2();
     }
-});
+}
+
+// atividade.addEventListener('click', (event) => {
+//     if (event.target.tagName !== 'INPUT') {
+//         atividade1();
+//         aluno2();
+//         turma2();
+//         disciplina2();
+//     }
+// });
 
 function disciplina1() {
     disciplina.innerHTML = `<div class="nome">
@@ -150,6 +213,7 @@ function disciplina1() {
     disciplina.style.width = "30vw";
     disciplina.style.height = "45vh";
     disciplina.style.transition = ".5s";
+    disciplina.style.border=".3vw solid #D9D9D9";
     aluno.style.left = "25%";
     aluno.style.top = "75%";
     aluno.style.transition = ".5s";
@@ -159,6 +223,7 @@ function disciplina1() {
     atividade.style.left = "50%";
     atividade.style.top = "85%";
     atividade.style.transition = ".5s";
+    disciplina.removeEventListener('click', disciplinaLarge)
 }
 
 function disciplina2() {
@@ -166,13 +231,24 @@ function disciplina2() {
     disciplina.style.width = "10vw";
     disciplina.style.height = "20vh";
     disciplina.style.backgroundImage = "none";
+    disciplina.style.border=".3vw solid #B50709";
+    disciplina.addEventListener('click',disciplinaLarge)
 }
 
-disciplina.addEventListener('click', (event) => {
-    if (event.target.tagName !== 'INPUT') {
+function disciplinaLarge (event){
+    if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'SELECT' && event.target.tagName !== 'BUTTON') {
         disciplina1();
         aluno2();
         turma2();
         atividade2();
     }
-});
+}
+
+// disciplina.addEventListener('click', (event) => {
+//     if (event.target.tagName !== 'INPUT') {
+//         disciplina1();
+//         aluno2();
+//         turma2();
+//         atividade2();
+//     }
+// });
